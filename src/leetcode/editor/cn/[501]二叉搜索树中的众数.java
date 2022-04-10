20520ef4b079251dfaa5238c6b94b0b6e35385ba
Lianyao Guo <guolianyao@outlook.com>
2022-04-10 16:46:07 +0800

    235

--- src/leetcode/editor/cn/[501]二叉搜索树中的众数.java
+++ src/leetcode/editor/cn/[501]二叉搜索树中的众数.java
@@ -16,10 +16,8 @@ package leetcode.editor.cn;
  * }
  */
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import javax.management.MXBean;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -29,47 +27,40 @@ import java.util.stream.Collectors;
  * 3. 由于可以在遍历的过程中就知道哪个频率最高，而非先遍历一遍统计所有词的频率，所以本解法可以不用map
  */
 class Solution {
-    private List<Integer> resList;
-    private int curCount;
-    private int maxCount;
-    TreeNode pre;
-
-    public Solution() {
-        resList = new ArrayList<>();
-        curCount = 0;
-        maxCount = 0;
-        pre = null;
-    }
-
     public int[] findMode(TreeNode root) {
-        searchBST(root);
-        // 需要把List转为int[]
-        return resList.stream().mapToInt(Integer::intValue).toArray();
-    }
+        TreeNode cur=root;
+        TreeNode pre=null;
+        List<Integer> resList=new ArrayList<>();
+        int curCount=0;
+        int maxCount=0;
 
-    private void searchBST(TreeNode cur) {
-        if (cur == null) return;
-        searchBST(cur.left);
-        // 一个一个遍历，相同的频率+1，不同的就归一继续统计新词频
-        if (pre == null || cur.val != pre.val) {
-            curCount = 1;
-        } else {
-            curCount++;
+        // 迭代法需要用到栈
+        Stack<TreeNode> stack=new Stack<>();
+        // 因为push结点的逻辑只能在循环内部，所以还需要cur!=null来启动循环
+        while(cur!=null||!stack.isEmpty()){
+            if(cur!=null){
+                stack.push(cur);
+                cur=cur.left;
+            }else{
+                cur=stack.pop();
+                if(pre==null||pre.val!=cur.val){
+                    curCount=1;
+                }else curCount++;
+
+                if(curCount> maxCount){
+                    resList.clear();
+                    resList.add(cur.val);
+                    maxCount=curCount;
+                }else if(curCount==maxCount){
+                    resList.add(cur.val);
+                }
+                // 先给pre赋值
+                pre=cur;
+                // 当前结点做完之后进行位移
+                cur=cur.right;
+            }
         }
-        // 因为是在遍历中，curCount有可能在之后会变，所以在当前结点的词频统计完后需要更新一下list中的信息
-        // 出现更大的得把原本答案队列中的最高频的词（可能有多个）清空,再加入新的最高频词
-        if (curCount > maxCount) {
-            resList.clear();
-            resList.add(cur.val);
-            maxCount = curCount;
-        }
-        // 达到最大词频就先加入结果集,之后没有更大的就是结果了
-        // 不能直接else，因为还有小于的情况
-        else if (curCount == maxCount) {
-            resList.add(cur.val);
-        }
-        pre = cur;
-        searchBST(cur.right);
+        return resList.stream().mapToInt(Integer::intValue).toArray();
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
